9c6eb8b4e8b7fd7e0a4401c3a471abc58cd17a65
JinGeun Lee <zealotjin@gmail.com>
2013-10-22 00:04:16 -0700

    chocolate solved

---
+++ chocolate/Chocolate.rb
@@ -0,0 +1,10 @@
+File.open(ARGV[0]).each_line do |line|
+
+
+
+
+
+
+
+
+end
---
+++ longestCommonSequence/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main (String[] args) {
+    File file = new File(args[0]);
+    BufferedReader in = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = in.readLine()) != null) {
+        String[] lineArray = line.split(";");
+        if (lineArray.length > 0) {
+            System.out.println("Hello");
+        }
+    }
+  }
+}
